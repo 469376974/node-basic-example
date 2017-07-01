0ecd41d3aeda5f50fa65d547162acc8d083adbc0
dengcong <469376974@qq.com>
2017-07-01 21:40:52 +0800

    删除多余注释代码

--- app.js
+++ app.js
@@ -113,54 +113,7 @@ app.post('/admin/movie/new', function (req, res) {
     }
 });
 
-
-// app.post('/admin/movie/new', function (res, req) {
-//     var id = req.body.movie._id;
-//     var movieObj = req.body.movie;
-//     var _movie = null;
-//
-//     console.log(req + 111);
-//     console.log(res + 222);
-//
-//     if (id !== 'undefined') {
-//         Movie.findById(id, function (err, movie) {
-//             if (err) {
-//                 console.log(err);
-//             }
-//
-//             _movie = _.extend(movie, movieObj);
-//             _moive.save(function (err, movie) {
-//                 if (err) {
-//                     console.log(err);
-//                 }
-//
-//                 res.redirect('/movie/' + movie._id)
-//             });
-//         })
-//     }
-//     else {
-//         _movie = new Movie({
-//             doctor: movieObj.doctor,
-//             title: movieObj.title,
-//             country: movieObj.country,
-//             language: movieObj.language,
-//             year: movieObj.year,
-//             poster: movieObj.poster,
-//             summary: movieObj.summary,
-//             flash: movieObj.flash
-//         });
-//
-//         _moive.save(function (err, movie) {
-//             if (err) {
-//                 console.log(err);
-//             }
-//
-//             res.redirect('/movie/' + movie._id);
-//         });
-//     }
-// });
-
-
+//admin list
 app.get('/admin/list', function (req, res) {
     Movie.fetch(function (err, movies) {
         if (err) {

6fecf8f406074b2dde63bb1dc684993d15f6d4c0
前端页面仔 <469376974@qq.com>
2017-07-01 13:06:01 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,175 @@
+/**
+ * Created by mac on 17/6/3.
+ */
+var express = require('express');
+var serveStatic = require('serve-static');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var _underscore = require('underscore');
+var Movie = require('./models/movie');
+var port = process.env.PORT || 3000;
+var app = express();
+
+mongoose.connect('mongodb://localhost:27017/imooc');
+
+app.set('views', './views/pages');
+app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(serveStatic('bower_components'));
+app.locals.moment = require('moment');
+app.listen(port);
+
+console.log('imooc started on port' + port);
+
+app.get('/', function (req, res) {
+    Movie.fetch(function (err, movies) {
+        if (err) {
+            console.log(err);
+        }
+        res.render('index', {
+            title: 'imooc 首页',
+            movies: movies
+        });
+    });
+});
+
+app.get('/movie/:id', function (req, res) {
+    var id = req.params.id;
+
+    Movie.findById(id, function (err, movie) {
+        res.render('detail', {
+            title: 'imooc' + movie.title,
+            movie: movie
+        });
+    });
+});
+
+app.get('/admin/movie', function (req, res) {
+    res.render('admin', {
+        title: 'imooc 后台录入页',
+        movie: {
+            doctor: '',
+            country: '',
+            title: '',
+            year: '',
+            poster: '',
+            language: '',
+            flash: '',
+            summary: ''
+        }
+    });
+});
+
+//admin update movie
+app.get('/admin/update/:id', function (req, res) {
+    var id =req.params.id;
+
+    if(id) {
+        Movie.findById(id, function (err, movie) {
+            res.render('admin', {
+                title: 'imooc 后台更新页',
+                movie: movie
+            })
+        })
+    }
+});
+
+//admin post movie
+app.post('/admin/movie/new', function (req, res) {
+    var id = req.body.movie._id;
+    var movieObj = req.body.movie;
+    var _movie = null;
+    if (id !== 'undefined') { // 已经存在的电影数据
+        Movie.findById(id, function (err, movie) {
+            if (err) {
+                console.log(err);
+            }
+            _movie = _underscore.extend(movie, movieObj);
+            // _movie = _underscore.extend(movie, movieObj); // 用新对象里的字段替换老的字段
+            _movie.save(function (err, movie) {
+                if (err) {
+                    console.log(err);
+                }
+                res.redirect('/movie/' + movie._id);
+            });
+        });
+    } else {  // 新加的电影
+        _movie = new Movie({
+            doctor: movieObj.doctor,
+            title: movieObj.title,
+            country: movieObj.country,
+            language: movieObj.language,
+            year: movieObj.year,
+            poster: movieObj.poster,
+            summary: movieObj.summary,
+            flash: movieObj.flash
+        });
+        _movie.save(function (err, movie) {
+            if (err) {
+                console.log(err);
+            }
+            res.redirect('/movie/' + movie._id);
+        });
+    }
+});
+
+
+// app.post('/admin/movie/new', function (res, req) {
+//     var id = req.body.movie._id;
+//     var movieObj = req.body.movie;
+//     var _movie = null;
+//
+//     console.log(req + 111);
+//     console.log(res + 222);
+//
+//     if (id !== 'undefined') {
+//         Movie.findById(id, function (err, movie) {
+//             if (err) {
+//                 console.log(err);
+//             }
+//
+//             _movie = _.extend(movie, movieObj);
+//             _moive.save(function (err, movie) {
+//                 if (err) {
+//                     console.log(err);
+//                 }
+//
+//                 res.redirect('/movie/' + movie._id)
+//             });
+//         })
+//     }
+//     else {
+//         _movie = new Movie({
+//             doctor: movieObj.doctor,
+//             title: movieObj.title,
+//             country: movieObj.country,
+//             language: movieObj.language,
+//             year: movieObj.year,
+//             poster: movieObj.poster,
+//             summary: movieObj.summary,
+//             flash: movieObj.flash
+//         });
+//
+//         _moive.save(function (err, movie) {
+//             if (err) {
+//                 console.log(err);
+//             }
+//
+//             res.redirect('/movie/' + movie._id);
+//         });
+//     }
+// });
+
+
+app.get('/admin/list', function (req, res) {
+    Movie.fetch(function (err, movies) {
+        if (err) {
+            console.log(err);
+        }
+
+        res.render('list', {
+            title: 'imooc 后台列表页',
+            movies: movies
+        });
+    });
+});

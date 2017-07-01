87e0e0aa59b2b2e6deae720899021b65db5401c9
dengcong <469376974@qq.com>
2017-07-01 22:32:57 +0800

    修复bug

--- public/js/admin.js
+++ public/js/admin.js
@@ -9,14 +9,14 @@ $(function () {
 
         $.ajax({
             type: 'DELETE',
-            url: '/admin/list?id' + id
-        }).done(function (res) {
-            if (res.success === 1) {
-                if (tr.length > 0) {
-                    tr.remove();
-                }
-            }
+            url: '/admin/list?id=' + id
         })
-
-    })
+            .done(function (results) {
+                if (results.success === 1) {
+                    if (tr.length > 0) {
+                        tr.remove();
+                    }
+                }
+            });
+    });
 });
